0bc5ea0a094d628ebb441953081baa83179ec028
jasonaul <jason.aul@gmail.com>
2022-07-06 19:38:02 -0400

    intervals for each item working (except for age)

--- js/app.js
+++ js/app.js
@@ -42,29 +42,75 @@ class TransporGatchi extends Transport {
 
 
 const transportSystem = new TransporGatchi ("k");
-
+const passengerButton = document.querySelector('#passengers')
+const scheduleButton = document.querySelector('#schedule')
+const maintenanceButton = document.querySelector('#maintenance')
 //Testing out the intervals
 
 const newGame = {
     setPassengers(){
-        const passInterval = setInterval(() => {
-            const passengerID = document.querySelector('#Passengers');
+        console.log("Inside setPassengers")
+        let passInterval = setInterval(() => {
+            let passengerID = document.querySelector('#Passengers-Carry');
+            console.log(passengerID)
         passengerID.innerHTML = `Passengers: ${transportSystem.passenger}`;
 
         transportSystem.passenger --; 
 
-        if(transportSystem.passenger <=0) {
-            alert(`You haven't kept up with demand! Your residents have REVOLTED.`);
+        if(transportSystem.passenger ===0) {
+            alert(`You haven't kept up with demand! Your residents have RIOTED.`);
             clearInterval(passInterval);
         }
-        }, 1000)  
+        }, 2000)  
         passengerButton.addEventListener("click", () =>{
             transportSystem.addPassenger();
-            const passengerID = document.querySelector ('#Passengers');
-            passInterval.textContent = `Passengers: ${transportSystem.passenger}`
+            const passengerID = document.querySelector ('#Passengers-Carry');
+            passInterval.innerHTML = `Passengers: ${transportSystem.passenger}`
         })
-    }
-    
+    },
+
+    setSchedule(){
+        console.log("Inside setSchedule")
+        let passInterval = setInterval(() => {
+            let scheduleID = document.querySelector('#Schedule-Show');
+            console.log(scheduleID)
+        scheduleID.innerHTML = `Schedule: ${transportSystem.ontime}`;
+
+        transportSystem.ontime --; 
+
+        if(transportSystem.ontime ===0) {
+            alert(`Your transportation system is woefully off-schedule! Your residents have RIOTED.`);
+            clearInterval(passInterval);
+        }
+        }, 2000)  
+        scheduleButton.addEventListener("click", () =>{
+            transportSystem.adjustSchedule();
+            const scheduleID = document.querySelector ('#Schedule-Show');
+            passInterval.innerHTML = `Schedule: ${transportSystem.ontime}`
+        })
+    },
+
+    setMaintenance(){
+          let passInterval = setInterval(() => {
+            let maintenanceID = document.querySelector('#Maintenance-Show');
+            maintenanceID.innerHTML = `Maintenance : ${transportSystem.maintenance}`;
+
+        transportSystem.maintenance ++; 
+
+        if(transportSystem.maintenance === 11) {
+            alert(`You failed to keep up with maintenance of your public transit system! Your residents have RIOTED.`);
+            clearInterval(passInterval);
+        }
+        }, 2000)  
+        maintenanceButton.addEventListener("click", () =>{
+            transportSystem.adjustMaintenance();
+            const maintenanceID = document.querySelector ('#Maintenance-Show');
+            passInterval.innerHTML = `Maintenance: ${transportSystem.maintenance}`
+        })
+    },
 }
 
-console.log(transportSystem.name)
+newGame.setPassengers()
+newGame.setSchedule()
+newGame.setMaintenance()
+console.log(transportSystem.passenger)
