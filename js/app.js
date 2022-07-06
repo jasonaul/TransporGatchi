1f9784a60eaae549ba20d3c57c4763305411549c
jasonaul <jason.aul@gmail.com>
2022-07-05 23:20:18 -0400

    Failing on intervals

--- js/app.js
+++ js/app.js
@@ -18,12 +18,13 @@ class Transport {
 // Class extends
 
 class TransporGatchi extends Transport {
-    constructor (name, passenger = 10, ontime = 10, maintenance = 0, age = 0){
+    constructor (name, passenger, ontime, maintenance, age){
+        super(name, passenger, ontime, maintenance, age)
         this.name = name;
-        this.passenger = passenger;
-        this.ontime = ontime;
-        this.maintenance = maintenance;
-        this.age = age;
+        this.passenger = 10;
+        this.ontime = 10;
+        this.maintenance = 0;
+        this.age = 0;
     }
     addPassenger(){
         this.passenger ++
@@ -37,3 +38,26 @@ class TransporGatchi extends Transport {
 }
 
 // I think we have the class situation set. 
+
+
+const transportSystem = new TransporGatchi ("")
+
+//Testing out the intervals
+
+const newGame = {
+    setPassenger(){
+        const passInterval = setInterval(() => {
+            const passengerID = document.querySelector("#Passengers");
+        passengerID.innerHTML = `Passengers: ${transportSystem.passenger}`;
+
+        transportSystem.passenger -- 
+
+        if(transportSystem.passenger <=0) {
+            alert(`You haven't kept up with demand! Your residents have REVOLTED.`);
+            clearInterval(passInterval);
+        }
+        }, 100) 
+    }
+}
+
+console.log(transportSystem.passenger)
